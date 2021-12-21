c4d7fa84f31eaadb85eb57113c850330415b5ec6
sx65 <96387206+sx65@users.noreply.github.com>
2021-12-21 14:08:13 +0300

    Update post.js

--- pages/scripts/post.js
+++ pages/scripts/post.js
@@ -1,5 +1,25 @@
-export default function ABOUT(){
-     return <div>
-        <h3>print("works")</h3>
-    </div>
+import Link from 'next/link'
+
+function Home() {
+  return (
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a>About Us</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/blog/hello-world">
+          <a>Blog Post</a>
+        </Link>
+      </li>
+    </ul>
+  )
 }
+
+export default Home
